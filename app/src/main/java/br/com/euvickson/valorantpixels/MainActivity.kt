a0c19fa909693e0eee79cfb04a4b5bb1771eecfd
Euvickson <victor.eugennio@gmail.com>
2023-02-12 11:10:59 -0300

    Top App bar and Modal Menu Created.
    TOOD: Need to create de modal menu itens and style them

--- app/src/main/java/br/com/euvickson/valorantpixels/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/valorantpixels/MainActivity.kt
@@ -1,43 +1,80 @@
 package br.com.euvickson.valorantpixels
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.euvickson.valorantpixels.ui.theme.ValorantPixelsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ValorantPixelsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val drawerState = rememberDrawerState(DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+
+                    TopAppBarAndModalMenu(drawerState, scope)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ValorantPixelsTheme {
-        Greeting("Android")
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    private fun TopAppBarAndModalMenu(
+        drawerState: DrawerState,
+        scope: CoroutineScope
+    ) {
+        ModalNavigationDrawer(drawerContent = {
+            Text(text = "Item 1")
+            Text(text = "Item 2")
+            Text(text = "Item 3")
+        }, drawerState = drawerState) {
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = {},
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Menu Icon"
+                                )
+                            }
+                        },
+                    )
+                },
+                content = {}
+            )
+        }
     }
 }
